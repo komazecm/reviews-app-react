4d109e88731608eae9111a432c7cdb5be460ed49
Masa Komazec <masakomazec9@gmail.com>
2021-11-25 17:34:30 +0100

    added a heading

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,8 @@ import Review from './Review';
 class Card extends React.Component {
     render() { 
         return <div className="container">
+            <h1 id="title">Reviews</h1>
+            <hr id="line" />
             <Review />
         </div>;
     }
